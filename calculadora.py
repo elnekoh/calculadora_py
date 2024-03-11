9890429827fb39dafaae5f6a201558d4200e37ac
Nicolas Ferioli <feriolinicolas@gmail.com>
2024-03-11 03:31:58 -0300

    First commit

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+def sumar(n1, n2):
+    return n1+n2
+
+def restar(n1, n2):
+    return n1-n2
+
+def multiplicar(n1, n2):
+    return n1*n2
+
+def dividir(n1, n2):
+    return n1/n2
+num1 = int(input("pone un numerito: "))
+num2 = int(input("pone otro numerito: "))
+
+print(dividir(num1,num2))
