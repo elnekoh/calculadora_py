097fa9d6ba9cb70f28b596eca160527f83d9ae57
Nicolas Ferioli <feriolinicolas@gmail.com>
2024-03-11 03:52:24 -0300

    Cambiada la funcion dividir, para que no se divida por 0

--- calculadora.py
+++ calculadora.py
@@ -8,8 +8,16 @@ def multiplicar(n1, n2):
     return n1*n2
 
 def dividir(n1, n2):
-    return n1/n2
+    if n2!=0:
+        return n1/n2
+    else:
+        print("no se puede dividir por cero!")
+        return(None)
+
 num1 = int(input("pone un numerito: "))
 num2 = int(input("pone otro numerito: "))
 
-print(dividir(num1,num2))
+resultado = dividir(num1,num2)
+
+if resultado is not None:
+    print(resultado)
